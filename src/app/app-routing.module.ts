502e65f7aa962965e007f43da29d7a5dec07acf1
unknown <dotnetmind12@iiht.tech>
2022-01-17 14:52:07 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserMenuComponent } from './sign-up/user-menu/user-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'log-in' , 
+    component : LogInComponent
+  },
+  {path : 'sign-up',
+  component : SignUpComponent
+  },
+  {
+    path : 'Home',
+    component : HomeComponent
+  },
+  {
+    path : 'user-menu',
+    component : UserMenuComponent
+  },
+  
+  {
+    path : '',
+    redirectTo : '/sign-up',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ToastrModule } from 'ngx-toastr';
+import { UserMenuComponent } from './sign-up/user-menu/user-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    SignUpComponent,
+    HomeComponent,
+    UserMenuComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      progressBar: true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
